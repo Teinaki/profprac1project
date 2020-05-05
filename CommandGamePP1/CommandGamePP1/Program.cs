3ad62fd625846c3ca7902a3015da0cff61ab1de6
Teinaki McFelin <MCFETK1@student.op.ac.nz>
2020-05-06 11:58:23 +1200

    Added in class 'Game' and 4 methods, from resource http://programmingisfun.com/learn/c-sharp-adventure-game/c_sharp_12_framework_summary/

--- CommandGamePP1/CommandGamePP1/Program.cs
+++ CommandGamePP1/CommandGamePP1/Program.cs
@@ -2,6 +2,29 @@
 
 namespace CommandGamePP1
 {
+    class Game
+    {
+        static bool Run = true;
+        static int Choice;
+        static bool Goal = false;
+
+        public static void Menu()
+        {
+
+        }
+        public static void Start()
+        {
+
+        }
+        public static void End()
+        {
+
+        }
+        public static void Play()
+        {
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
