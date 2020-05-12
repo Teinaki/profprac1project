92030ce6bac447ddfc4ee3635bc1b18bd7712ea2
Teinaki McFelin <MCFETK1@student.op.ac.nz>
2020-05-12 17:21:33 +1200

    Formatted into methods, removed unused ones

--- CommandGamePP1/CommandGamePP1/Program.cs
+++ CommandGamePP1/CommandGamePP1/Program.cs
@@ -2,102 +2,87 @@
 
 namespace CommandGamePP1
 {
-    class Game
-    {
-        static bool Run = true;
-        static int Choice;
-        static bool Goal = false;
-
-        public static void Menu()
-        {
-
-        }
-        public static void Start()
-        {
-
-        }
-        public static void End()
-        {
-
-        }
-        public static void Play()
-        {
-
-        }
-    }
     class Program
     {
-        static void Main(string[] args)
+        public static int room = 0;
+        public static void story()
         {
-            // Just testing the updates
-
-            int room = 0;
+            switch (room)
+            {
+                case 0:
+                    Console.WriteLine("Pick a path. 1-6 // room 0//");
+                    break;
+                case 1:
+                    Console.WriteLine("Pick a path. 1-6 // room 1//");
+                    break;
+                default:
+                    Console.WriteLine("Pick a path. 1-6");
+                    break;
+            }
+        }
+        public static void choices()
+        {
 
             string[] correctpath = { "6", "4", "6", "4", "4", "3", "4", "5", "4", "2", "3", "1", "2", "4", "4" };
             // 646443454231244 is the maze image path to the end
+            string path = Console.ReadLine();
+            switch (path)
+            {
+                case "1":
+                    if (path == correctpath[room])
+                    {
+                        Console.WriteLine("Correct path");
+                        room++;
+                    }
+                    break;
+                case "2":
+                    if (path == correctpath[room])
+                    {
+                        Console.WriteLine("Correct path");
+                        room++;
+                    }
+                    break;
+                case "3":
+                    if (path == correctpath[room])
+                    {
+                        Console.WriteLine("Correct path");
+                        room++;
+                    }
+                    break;
+                case "4":
+                    if (path == correctpath[room])
+                    {
+                        Console.WriteLine("Correct path");
+                        room++;
+                    }
+                    break;
+                case "5":
+                    if (path == correctpath[room])
+                    {
+                        Console.WriteLine("Correct path");
+                        room++;
+                    }
+                    break;
+                case "6":
+                    if (path == correctpath[room])
+                    {
+                        Console.WriteLine("Correct path");
+                        room++;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Wrong Path value try again");
+                    break;
+            }                 // If you pick the right path you reach the next 'room' in the maze and go again
+        }
+        static void Main(string[] args)
+        {
 
             do
             {
-                switch (room)
-                {
-                    case 0:
-                        Console.WriteLine("Pick a path. 1-6 // room 0//");
-                        break;
-                    case 1:
-                        Console.WriteLine("Pick a path. 1-6 // room 1//");
-                        break;
-                }
-                string path = Console.ReadLine();
+                story();
+                choices();
                 Console.WriteLine();
-                switch (path)
-                {
-                    case "1":
-                        if (path == correctpath[room])
-                        {
-                            Console.WriteLine("Correct path");
-                            room++;
-                        }
-                        break;
-                    case "2":
-                        if (path == correctpath[room])
-                        {
-                            Console.WriteLine("Correct path");
-                            room++;
-                        }
-                        break;
-                    case "3":
-                        if (path == correctpath[room])
-                        {
-                            Console.WriteLine("Correct path");
-                            room++;
-                        }
-                        break;
-                    case "4":
-                        if (path == correctpath[room])
-                        {
-                            Console.WriteLine("Correct path");
-                            room++;
-                        }
-                        break;
-                    case "5":
-                        if (path == correctpath[room])
-                        {
-                            Console.WriteLine("Correct path");
-                            room++;
-                        }
-                        break;
-                    case "6":
-                        if (path == correctpath[room])
-                        {
-                            Console.WriteLine("Correct path");
-                            room++;
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Wrong Path value try again");
-                        break;
-                }
-                // If you pick the right path you reach the next 'room' in the maze and go again
             } while (room < 15);
             // You're struck in this maze until you get the right path to the end.
             Console.WriteLine("You reached the end of the maze");
