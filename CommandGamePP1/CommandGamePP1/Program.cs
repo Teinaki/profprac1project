77542c3a90a85874c0493b83e0ee5c67d9bbadc8
ihipera <64507442+ihipera@users.noreply.github.com>
2020-05-19 10:00:30 +1200

    Revert "Merge branch 'master' of https://github.com/Teinaki/profprac1project"
    This reverts commit 3d4d4692c07487a753b1a0ef6f9bb1459a130c15, reversing
    changes made to 1aad18c131bdd7d8f3dca7e9b45408a477163328.

--- CommandGamePP1/CommandGamePP1/Program.cs
+++ CommandGamePP1/CommandGamePP1/Program.cs
@@ -5,69 +5,72 @@ namespace CommandGamePP1
 {
     class Program
     {
-        public static int room = 0,orangekey = 0, greenkey = 0, redkey = 0, yellowkey = 0;  //rooms 3,5,6,9,10,11,14. need keys, maybe room 6 will lead down long path to nowhere
+        public static int room = 0;  //rooms 3,5,6,9,10,11,14. need keys, maybe room 6 will lead down long path to nowhere
         public static void story()
         {
             switch (room)
             {
                 case 0:
-                    Console.WriteLine("Press the corresponding number to go through \n1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.");
+                    Console.WriteLine("Oh no! You have found yourself trapped in a maze.");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("You see multiple doors in front of you. Select from the following:    ");
+                    Console.WriteLine("Press the corresponding number to go through \n1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door.");
                     break;
                 case 1:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 2:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 3:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 4:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 5:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 6:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 7:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 8:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 9:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 10:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 11:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 12:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) Go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 13:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Search room.  6) go back. ");
+                    Console.WriteLine("1) Red door.  2) Orange door.  3) Yellow door.  4) Green door.  5) Blue door.  6) Purple door. ");
                     break;
                 case 14:
                     Console.WriteLine("Select from the following:    ");
-                    Console.WriteLine("1) Gold  door. 2) Gold door. 3) Gold door. 4) Jeff 5) Search room. 6) Go back.");
+                    Console.WriteLine("1) Gold  door. 2) Gold door. 3) Gold door. 4) Jeff 5) Gold door. 6) Gold door.");
                     break;
                 default:
                     Console.WriteLine("Pick a path. 1-6");
@@ -78,171 +81,112 @@ namespace CommandGamePP1
         public static void choices()
         {
 
-            string[] correctpath = { "4", "2", "4", "2", "2", "1", "2", "3", "2", "1", "4", "1", "1", "2", "2" };
-            // 424221232141122 is the maze image path to the end
+            string[] correctpath = { "6", "4", "6", "4", "4", "3", "4", "5", "4", "2", "3", "1", "2", "4", "4" };
+            // 646443454231244 is the maze image path to the end
             string path = Console.ReadLine();
             switch (path)
             {
                 case "1":
                     if (path == correctpath[room])
                     {
-                        switch (room)
+                        if (room != 11)
                         {
-                            case 5:
-                                if (redkey == 1)
-                                {
-                                    Console.WriteLine("You use the redkey to unlock the door");
-                                    room++;
-                                    redkey--;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Door is locked");
-                                }
-                                break;
-                            case 9:
-                                if (redkey == 1)
-                                {
-                                    Console.WriteLine("You use the redkey to unlock the door");
-                                    room++;
-                                    redkey--;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Door is locked");
-                                }
-                                break;
-                            case 11:
-                                if (redkey == 1)
-                                {
-                                    Console.WriteLine("You use the redkey to unlock the door");
-                                    room++;
-                                    redkey--;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Door is locked");
-                                }
-                                break;
-                            default:
-                                Console.WriteLine("Correct Path");
-                                room++;
-                                break;
+                            Console.WriteLine("Correct path");
+                            room++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Door is locked"); //Stuck right now, need to work on key code to progress maze
                         }
                     }
                     break;
                 case "2":
                     if (path == correctpath[room])
                     {
-                        switch (room)
+                        if (room != 9)
                         {
-                            case 3:
-                                if (orangekey == 1)
-                                {
-                                    Console.WriteLine("You use the orange to unlock the door");
-                                    room++;
-                                    orangekey--;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Door is locked");
-                                }
-                                break;
-                            case 6:
-                                if (orangekey == 1)
-                                {
-                                    Console.WriteLine("You use the orange to unlock the door");
-                                    room++;
-                                    orangekey--;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Door is locked");
-                                }
-                                break;
-                            case 14:
-                                if (orangekey == 1)
-                                {
-                                    Console.WriteLine("You use the orange to unlock the door");
-                                    room++;
-                                    orangekey--;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Door is locked");
-                                }
-                                break;
-                            default:
-                                Console.WriteLine("Correct Path");
-                                room++;
-                                break;
+                            Console.WriteLine("Correct path");
+                            room++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Door is locked"); //Stuck right now, need to work on key code to progress maze
                         }
                     }
                     break;
                 case "3":
                     if (path == correctpath[room])
                     {
-                        Console.WriteLine("Correct Path");
-                        room++;
+                        if (room != 5 && room != 10)
+                        {
+                            Console.WriteLine("Correct path");
+                            room++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Door is locked"); //Stuck right now, need to work on key code to progress maze
+                        }
                     }
                     break;
                 case "4":
                     if (path == correctpath[room])
                     {
-                        switch (room)
+                        if (room != 3 && room != 6 && room != 14)
                         {
-                            case 10:
-                                if (greenkey == 1)
-                                {
-                                    Console.WriteLine("You use the greenkey to unlock the door");
-                                    room++;
-                                    redkey--;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Door is locked");
-                                }
-                                break;
-                            default:
-                                Console.WriteLine("Correct Path");
-                                room++;
-                                break;
+                            Console.WriteLine("Correct path");
+                            room++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Door is locked"); //Stuck right now, need to work on key code to progress maze
                         }
                     }
                     break;
                 case "5":
-                    Console.WriteLine("You search the room");
-                    switch (room)
+                    if (path == correctpath[room])
                     {
-                        case 1:
-                            Console.WriteLine("You find a orange key");
-                            orangekey++;
-                            break;
-                        case 4:
-                            break;
-                        case 5:
-                            break;
-                        default:
-                            Console.WriteLine("Room was empty");
-                            break;
+                        Console.WriteLine("Correct path");
+                        room++;
                     }
                     break;
                 case "6":
-                    Console.WriteLine("You decide to move back a room");
-                    room--;
+                    if (path == correctpath[room])
+                    {
+                        Console.WriteLine("Correct path");
+                        room++;
+                    }
                     break;
                 default:
                     Console.WriteLine("Wrong Path value try again");
                     break;
             }                 // If you pick the right path you reach the next 'room' in the maze and go again
         }
+        public static void search()
+        {
+            switch (room)
+            {
+                case 1:
+                    Console.WriteLine("You enter a dimly lit room");
+                    Console.WriteLine("Would you like to search the room?");
+                    string temp = Console.ReadLine();
+                    switch (temp)
+                    {
+                        case "yes":
+                        case "Yes":
+                        case "y":
+                            Console.WriteLine("You find key or something");
+                            // then do yellowkey++ or whatever
+                            break;
+                    }
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Oh no! You have found yourself trapped in a maze.");
-            Thread.Sleep(2000);
-            Console.WriteLine("You see multiple doors in front of you. Select from the following:    ");
+
             do
             {
+                search();
                 story();
                 choices();
                 Console.WriteLine();
